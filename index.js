393c40c9f772b4374922df3b19c86bd658add4fc
AbaadirYasin <abaadir.yasin@student.moringaschool.com>
2023-10-05 11:04:01 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-// Function expression called divide
 const divide = function() {
     return 2000 / 100;
   }
   
-  // Arrow function called square
   const square = (x) => {
     return x * x;
   }
   
-  // Arrow function called add
   const add = (x, y) => {
     return x + y;
   }
   
-  // Export the functions (if needed)
   module.exports = {
     divide,
     square,
